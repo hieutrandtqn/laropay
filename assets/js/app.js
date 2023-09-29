bba170fafd350434c34f5fa9a3fb632cb7cf7bf2
tronghieu2909 <tronghieu2909@gmail.com>
2023-09-29 17:48:47 +0700

    Add 404 page

--- assets/js/app.js
+++ assets/js/app.js
@@ -501,4 +501,18 @@
     $(".page-info").css({ display: "none" });
     $(".category-result, .search-result, .not-found, .not-found-desc").remove();
   }
+
+  let container1 = document.getElementById('Square');
+    window.onmousemove = function (e) {
+        let x = -e.x / 90,
+            y = -e.y / 90;
+
+        container1.style.right = x + 'px';
+        container1.style.bottom = y + 'px';
+    }
+    /* Mobile gyroscope */
+    window.addEventListener("deviceorientation", function (e) {
+        container1.style.right = e.gamma/3 + "px"
+        container1.style.bottom = e.beta/3 + "px"
+    })
 })(jQuery);
